99e313a8a05abc6db1e03325f83dd2d2beb10c77
cxnt <pavelbartsits@gmail.com>
2022-09-13 19:47:03 +0300

    update input widget demo

--- 012_input/src/main.py
+++ 012_input/src/main.py
@@ -11,7 +11,7 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 api = sly.Api()
 app = sly.Application(templates_dir=os.path.join(os.getcwd(), "012_input", "templates"))
 
-input_text = sly.app.widgets.Input(value="AACCXX", placeholder="Type: Text")
+input_text = sly.app.widgets.Input(placeholder="Please input")
 button_random_planet = sly.app.widgets.Button(text="Random planet name")
 button_clean_input = sly.app.widgets.Button(text="Clean input")
 button_set_readonly = sly.app.widgets.Button(text="Set readonly")
@@ -42,8 +42,9 @@ def random_word():
 
 @button_set_readonly.click
 def set_readonly():
-    if input_text.readonly:
-        input_text.readonly = False
+    if input_text.is_readonly():
+        input_text.disable_readonly()
+        print("Readonly: Disabled")
     else:
-        input_text.readonly = True
-
+        input_text.enable_readonly()
+        print("Readonly: Enabled")
