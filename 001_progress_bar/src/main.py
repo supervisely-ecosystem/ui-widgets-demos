dd68b0f2f30114c7a2ca8b657a200f17ca708b2b
cxnt <pavelbartsits@gmail.com>
2022-10-05 20:29:31 +0400

    make no html mini app

--- 001_progress_bar/src/main.py
+++ 001_progress_bar/src/main.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 import supervisely as sly
+from supervisely.app.widgets import Card, Container
 
 
 # for convenient debug, has no effect in production
@@ -9,15 +10,19 @@ load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
-app = sly.Application(
-    templates_dir=os.path.join(os.getcwd(), "001_progress_bar", "templates")
-)
-
+# app = sly.Application(
+#     templates_dir=os.path.join(os.getcwd(), "001_progress_bar", "templates")
+# )
 
 # initialize widgets we will use in UI
 progress = sly.app.widgets.Progress(hide_on_finish=False)
 button = sly.app.widgets.Button(text="Start", icon="zmdi zmdi-play")
 finish_msg = sly.app.widgets.Text(status="success")
+
+card = Card("Progress Bar", content=Container([progress, button, finish_msg]))
+
+layout = Container(widgets=[card], direction="vertical")
+app = sly.Application(layout=layout)
 
 
 @button.click
