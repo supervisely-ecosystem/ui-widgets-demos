5c2a2f51e5a4eb83c6779e48fafb8c8bbe0b3ae7
cxnt <pavelbartsits@gmail.com>
2022-11-15 16:39:11 +0400

    Widgets 19-20 done

--- 019_done_label/src/main.py
+++ 019_done_label/src/main.py
@@ -1,3 +1,24 @@
-import supervisely as sly
+import os
 
-# File will be populated with widget example soon
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.app.widgets import Card, Container, DoneLabel
+
+
+# for convenient debug, has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api = sly.Api()
+
+# initialize widgets we will use in UI
+done_label = DoneLabel(
+    text="Task has been successfully finished",
+)
+
+card = Card(
+    title="Done Label",
+    content=done_label,
+)
+layout = Container(widgets=[card])
+app = sly.Application(layout=layout)
