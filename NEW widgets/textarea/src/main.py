c8f648723ca85c4b2d7ae8f93ab5902c25d85094
ruslantau <ruslan042743@gmail.com>
2022-12-07 17:32:36 +0700

    Textarea methods added.

--- NEW widgets/textarea/src/main.py
+++ NEW widgets/textarea/src/main.py
@@ -3,14 +3,15 @@ import os
 import numpy as np
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Card, LineChart, Container, Grid, Textarea
+from supervisely.app.widgets import Card, Textarea
 
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
-textarea = Textarea(text="Some text in text area.\nThat element can display multiple rows of text.\nThird sentence.", 
+textarea = Textarea(value="Some text in text area.\nThat element can display multiple rows of text.\nThird sentence.", 
                     autosize=False)
+
 card = Card(title="My card", content=textarea)
 app = sly.Application(layout=card)
