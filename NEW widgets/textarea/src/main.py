a9c46a479a1ec98398f21dc0264501f05dfaf909
ruslantau <ruslan042743@gmail.com>
2022-12-07 18:10:05 +0700

    Demo updated

--- NEW widgets/textarea/src/main.py
+++ NEW widgets/textarea/src/main.py
@@ -3,15 +3,19 @@ import os
 import numpy as np
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Card, Textarea
+from supervisely.app.widgets import Card, TextArea, Container
 
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
-textarea = Textarea(value="Some text in text area.\nThat element can display multiple rows of text.\nThird sentence.", 
-                    autosize=False)
+text_area_1 = TextArea(value="Some text in the text area.\nThat widget can display multiple rows of text.\nThird sentence.", 
+                       autosize=False)
 
-card = Card(title="My card", content=textarea)
+text_area_2 = TextArea(value="This is the same widget, but with autosize=True. \nDue to this, it stretches under the content. \nYou don't have to scroll to see all the content.", 
+                       autosize=True)
+
+container = Container(widgets=[text_area_1, text_area_2])
+card = Card(title="My card", content=container)
 app = sly.Application(layout=card)
