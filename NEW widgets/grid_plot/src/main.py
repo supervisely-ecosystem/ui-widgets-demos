a9966901079a33958946a12a1774d6abb3a403ba
ruslantau <ruslan042743@gmail.com>
2022-12-15 20:24:51 +0700

    Small fixes

--- NEW widgets/grid_plot/src/main.py
+++ NEW widgets/grid_plot/src/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import supervisely as sly
 from dotenv import load_dotenv
 from supervisely.app.content import StateJson, DataJson
-from supervisely.app.widgets import Card, Container, LinePlot, GridPlot, Button
+from supervisely.app.widgets import Card, Container, Grid, LinePlot, Button
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
@@ -16,7 +16,6 @@ def get_points(size: int=10, x_counter = -1, increasing=False):
         return [{"x": x + x_counter + 1, "y": np.random.normal(loc=0.5, scale=1.0) + np.sqrt(x + x_counter + 1)} for x in range(size)]
     else:
         return [{"x": x + x_counter + 1, "y": np.random.normal(loc=0.5, scale=1.0) - np.sqrt(x + x_counter + 1)} for x in range(size)]
-
 
 
 line_plot_1 = LinePlot(
@@ -40,9 +39,6 @@ line_plot_5 = LinePlot(
     series=[{"name": "Train", "data": []}, {"name": "Val", "data": []}],
 )
 
-grid_plot = GridPlot(widgets=[line_plot_1, line_plot_2, line_plot_3, line_plot_4, line_plot_5], columns=3)
-card = Card(title="Card with grid of lineplots", content=grid_plot)
-
 generate = {'val': False}
 button_run_generation = Button('Run losses generation (Freq = 0.1 sec)')
 @button_run_generation.click
@@ -59,13 +55,12 @@ def add_new_point_to_series():
                 line_plot.add_to_series(name_or_id=series['name'], data=get_points(1, x_counter=x_max, increasing=False))
         sleep(0.1)
 
-generate2 = {'val': False}
 button_run_generation2 = Button('Run metrics generation (Freq = 5 sec)')
 @button_run_generation2.click
-def add_new_point_to_series():
+def add_new_point_to_series2():
     print('Generation running')
-    generate2['val'] = True
-    while generate2['val']:
+    generate['val'] = True
+    while generate['val']:
         for line_plot in (line_plot_4, line_plot_5):
             for series in DataJson()[line_plot.widget_id]['series']:
                 if len(series['data']) > 0:
@@ -75,13 +70,14 @@ def add_new_point_to_series():
                 line_plot.add_to_series(name_or_id=series['name'], data=get_points(1, x_counter=x_max, increasing=True))
         sleep(5)
 
-button_stop_generation = Button('Stop series generation')
+button_stop_generation = Button('Stop generation')
 @button_stop_generation.click
-def add_new_point_to_series():
+def stop_generation():
     generate['val'] = False
-    generate2['val'] = False
     print('Generation stopped')
 
+grid_plot = Grid(widgets=[line_plot_1, line_plot_2, line_plot_3, line_plot_4, line_plot_5], columns=3)
+card = Card(title="Card with grid of lineplots", content=grid_plot)
 buttons_container = Container([button_run_generation, button_run_generation2, button_stop_generation], direction='horizontal')
 container = Container([card, buttons_container])
 app = sly.Application(layout=container)
