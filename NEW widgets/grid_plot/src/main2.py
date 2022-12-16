be877c3fb7ddd37d80cf7ce15246f186b000eebc
ruslantau <ruslan042743@gmail.com>
2022-12-16 23:30:14 +0700

    Global vars added

--- NEW widgets/grid_plot/src/main2.py
+++ NEW widgets/grid_plot/src/main2.py
@@ -1,4 +1,6 @@
 import os
+import asyncio
+import concurrent
 from time import sleep
 
 import numpy as np
@@ -10,64 +12,72 @@ load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
+executor = concurrent.futures.ThreadPoolExecutor()
+loop = asyncio.get_event_loop()
 
-def get_y(x, increasing=False): 
+def get_pseudo_random(x, increasing=False): 
     if increasing:
         return np.random.normal(loc=0.5, scale=1.0) + np.sqrt(x)
     else:
         return np.random.normal(loc=0.5, scale=1.0) - np.sqrt(x)
 
-def get_points(size: int=10, x_counter = -1, increasing=False):
-    if increasing:
-        return [{"x": x + x_counter + 1, "y": get_y(x, increasing)} for x in range(size)]
-    else:
-        return [{"x": x + x_counter + 1, "y": get_y(x, increasing)} for x in range(size)]
-
-plots_list_of_dicts = [
-    {"title":"Box loss", "series":[]},
-    {"title":"Obj loss", "series":[]},
-    {"title":"Cls loss", "series":[]},
-    {"title":"Pr + Rec", "series":[]},
-    {"title":"mAP",      "series":[]},
+plots_config = [
+    {"title":"Box loss", "show_legend": False}, 
+    {"title":"Obj loss", "show_legend": True}, 
+    {"title":"Cls loss", "show_legend": False}, 
+    {"title":"Pr + Rec", "show_legend": True}, 
+    {"title":"mAP", "show_legend": False}
 ]
-plots_list_of_names = ["Box loss", "Obj loss", "Cls loss", "Pr + Rec", "mAP"]
+generate = False #  just flag for control plot points generation 
+losses_x =  0 # last value for x-axis of losses plots
+metrics_x = 0  # last value for x-axis of metrics plots
+grid_plot = GridPlot(plots_config, columns=3)
 
-grid_plot = GridPlot(plots_list_of_names, columns=3)
+def long_task():
+    print('Generation running')
+    global generate, losses_x
+    generate = True
+    while generate:
+        for plot_title in ('Box loss', 'Obj loss', 'Cls loss', 'Box loss'):
+            grid_plot.add_scalars(plot_title, {'Train': get_pseudo_random(losses_x), 'Val': get_pseudo_random(losses_x)}, losses_x)
+        losses_x += 1
+        sleep(0.1)
 
-card = Card(title="Card with grid of lineplots", content=grid_plot)
+def callback(future):
+    if future.exception():
+        print(repr(future.exception()))
+    else:
+        print(future.result())
+    button_run_generation.loading = False
 
-generate = {'val': False, 'losses_last_x': 0, 'metrics_last_x': 0, }
 button_run_generation = Button('Run losses generation (Freq = 0.1 sec)')
 @button_run_generation.click
 def run_losses_generation():
     print('Generation running')
-    generate['val'] = True
-    while generate['val']:
-        grid_plot.add_scalars('Box loss', {'Train': get_y(generate["losses_last_x"])}, generate["losses_last_x"])
-        grid_plot.add_scalars('Obj loss', {'Train': get_y(generate["losses_last_x"]), 'Val': get_y(generate["losses_last_x"])}, generate["losses_last_x"])
-        grid_plot.add_scalars('Cls loss', {'Train': get_y(generate["losses_last_x"]), 'Val': get_y(generate["losses_last_x"])}, generate["losses_last_x"])
-        grid_plot.add_scalars('Box loss', {'Train': get_y(generate["losses_last_x"]), 'Val': get_y(generate["losses_last_x"])}, generate["losses_last_x"])
-        generate["losses_last_x"] += 1
-        sleep(0.1)
+    my_future = loop.run_in_executor(executor, long_task)
+    task = asyncio.ensure_future(my_future, loop=loop)
+    task.add_done_callback(callback)
 
 button_run_generation2 = Button('Run metrics generation (Freq = 5 sec)')
 @button_run_generation2.click
 def run_metrics_generation():
     print('Generation running')
-    generate['val'] = True
-    while generate['val']:
-        grid_plot.add_scalar('Pr + Rec/Train', get_y(generate["metrics_last_x"], increasing=True), generate["metrics_last_x"])
-        grid_plot.add_scalar('mAP/Train', get_y(generate["metrics_last_x"], increasing=True), generate["metrics_last_x"])
-        generate["metrics_last_x"] += 1
+    global generate, metrics_x
+    generate = True
+    while generate:
+        for plot_title in ('Pr + Rec/Train', 'mAP/Train'):
+            grid_plot.add_scalar(plot_title, get_pseudo_random(metrics_x, increasing=True), metrics_x)
+        metrics_x += 1
         sleep(5)
-
 
 button_stop_generation = Button('Stop generation')
 @button_stop_generation.click
 def stop_generation():
-    generate['val'] = False
+    global generate
+    generate = False
     print('Generation stopped')
 
+card = Card(title="Card with grid of lineplots", content=grid_plot)
 buttons_container = Container([button_run_generation, button_run_generation2, button_stop_generation], direction='horizontal')
 container = Container([card, buttons_container])
 app = sly.Application(layout=container)
