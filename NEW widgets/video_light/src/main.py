d98804a7dd785910af1f11adb8c63092dea2d9f0
Almaz <79905215+almazgimaev@users.noreply.github.com>
2022-12-30 15:05:00 +0300

    Video player widget updated (play/pause buttons) (#19)

--- NEW widgets/video_light/src/main.py
+++ NEW widgets/video_light/src/main.py
@@ -11,38 +11,61 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
-video_id = 17546937
 
-# get video info from server
-video_info = api.video.get_info_by_id(video_id)
-video_url = abs_url(video_info.path_original)
-video_type = video_info.file_meta["mime"]
+# prepare video and video info
+video_url = "https://user-images.githubusercontent.com/79905215/210067166-e5531dae-d090-436e-bb3b-f053e2e831eb.mp4"
+video_type = "video/mp4"
+video_meta = sly.video.get_info(video_url)
 
 # initialize widgets we will use in UI
 video1 = VideoPlayer(url=video_url, mime_type=video_type)
 video2 = VideoPlayer()
 video2.set_video(url=video_url, mime_type=video_type)
 
-# create control form
+# create play/pause buttons
+play_btn = Button(text="Play", button_size="mini", icon="zmdi zmdi-play")
+pause_btn = Button(text="Pause", button_size="mini", icon="zmdi zmdi-pause")
+
+# create current time control form
 get_time_btn = Button(text="Get timestamp", button_size="mini")
-input_time = InputNumber(value=0, min=0, max=video_info.duration)
+input_time = InputNumber(
+    value=0,
+    min=0,
+    max=round(video_meta["duration"], 1),
+    step=0.1,
+)
 set_time_btn = Button(text="Set timestamp", button_size="mini")
 
 # create containers for control form
-controls_container = Flexbox(widgets=[get_time_btn, input_time, set_time_btn], center_content=True)
+controls_container = Flexbox(
+    widgets=[play_btn, pause_btn, get_time_btn, input_time, set_time_btn],
+    center_content=True,
+)
 
 # create new cards
 card1 = Card(
-    title="Video widget",
+    title="Video player",
     content=video1,
 )
 card2 = Card(
-    title="Get or set current video timestamp",
+    title="Controls - operations from python code",
     content=Container(widgets=[video2, controls_container]),
 )
 
 layout = Container(widgets=[card1, card2], direction="horizontal", fractions=[1, 1])
 app = sly.Application(layout=layout)
+
+
+# start playing video
+@play_btn.click
+def play():
+    video2.play()
+
+
+# pause video
+@pause_btn.click
+def pause():
+    video2.pause()
 
 
 # get current timestamp
