2be1b72002fd9ec3480b156de9e2aabec6e1a1d2
almaz_macbook <almazgimaev@gmail.com>
2023-01-27 16:14:16 +0300

    added more comments

--- 046_file_thumbnail/src/main.py
+++ 046_file_thumbnail/src/main.py
@@ -13,17 +13,24 @@ api = sly.Api()
 
 team_id = env.team_id()
 
+# prepare file to upload in Team files
 local_filepath = "/Users/almaz/job/text.txt"
+
+# remote path in Team files
 remote_filepath = "/file_thumbnail_demo/text.txt"
 
 if api.file.exists(team_id, remote_filepath):
     api.file.remove(team_id, remote_filepath)
+
+# upload file from local directory to Team files
 api.file.upload(team_id, local_filepath, remote_filepath)
 
+# get file info from server
 fileinfo = api.file.get_info_by_path(team_id, remote_filepath)
 
-# initialize widgets we will use in UI
+# initialize widgets we will use in UI and set FileInfo
 file_thumbnail = FileThumbnail(info=fileinfo)
+
 card = Card(
     title="File Thumbnail",
     content=Container(widgets=[file_thumbnail]),
