3178344aacec94ea512e19dd4a136719fd405631
MaxTeselkin <tesmaxxxx@gmail.com>
2023-02-10 14:58:41 +0300

    updated select_app_session

--- 050_select_app_session/src/main.py
+++ 050_select_app_session/src/main.py
@@ -8,8 +8,9 @@ from supervisely.app.widgets import Card, Container, SelectAppSession
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+team_id = sly.env.team_id()
 # initialize widgets we will use in UI
-select_app_session = SelectAppSession()
+select_app_session = SelectAppSession(team_id=team_id, tags=["deployed_nn"])
 
 card = Card(
     title="Select App Session",
