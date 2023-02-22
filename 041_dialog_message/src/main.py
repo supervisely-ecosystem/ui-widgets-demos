b756c7506efd31ed622333301d1b025f4e6b4e8e
peixed351 <120389559+peixed351@users.noreply.github.com>
2023-02-22 16:45:27 +0300

    Widgets 41 50 (#50)
    * update widget 41
    
    * add 42 readmy
    
    * add 43 readmy
    
    * add 44 readmy
    
    * add 45 readmy
    
    * some check
    
    * add 46 readmy
    
    * add 47 readmy
    
    * add 48 readmy
    
    * add 49 readmy
    
    * add 50 readmy
    
    * some check
    
    * add 41 readmy
    
    * some check
    
    * check Literal None
    
    * correct small errors
    
    ---------
    
    Co-authored-by: cxnt <pavelbartsits@gmail.com>
    Co-authored-by: Pavel Bartsits <48913536+cxnt@users.noreply.github.com>

--- 041_dialog_message/src/main.py
+++ 041_dialog_message/src/main.py
@@ -12,13 +12,13 @@ api = sly.Api()
 
 # initialize widgets we will use in UI
 
-info_btn = Button("Show info")
-warning_btn = Button("Show warning")
-error_btn = Button("Show error")
-success_btn = Button("Show success")
+info_btn = Button("Show info", button_type="info")
+success_btn = Button("Show success", button_type="success")
+warning_btn = Button("Show warning", button_type="warning")
+error_btn = Button("Show error", button_type="danger")
 
 card = Card(
-    title="Dialog message demo",
+    title="Dialog message",
     description="click button to show dialog window",
     content=Flexbox([info_btn, success_btn, warning_btn, error_btn]),
 )
@@ -28,7 +28,7 @@ app = sly.Application(layout=card)
 
 @info_btn.click
 def show_info():
-    sly.app.show_dialog(title="Hello", description="some message", status="info")
+    sly.app.show_dialog(title="Hello", description="Info description", status="info")
 
 
 @success_btn.click
@@ -38,13 +38,12 @@ def show_success():
 
 @warning_btn.click
 def show_waring():
-    sly.app.show_dialog(title="My warning", description="some warning", status="warning")
+    sly.app.show_dialog(title="My warning", description="Warning description", status="warning")
     # or
     # raise sly.app.DialogWindowWarning(title="My warning", description="Warning description")
 
 
 @error_btn.click
 def show_error():
-    sly.app.show_dialog(title="My error", description="Error description", status="error")
-    # or
+    sly.app.show_dialog(title="My error", description="Error description", status="error")  # or
     # raise sly.app.DialogWindowError(title="My error", description="Error description")
