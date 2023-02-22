0423243bcdc7932e87ad5012a209a15c7259ac03
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-02-22 16:41:15 +0300

    16-25 widgets readme (#48)
    * checkbox readme
    
    * OneOf readme
    
    * NotificationBox readme
    
    * fix images
    
    * added image
    
    * DoneLabel readme
    
    * ObjectClassView readme
    
    * Empty readme
    
    * changed demo
    
    * Table readme and demo
    
    * fix images
    
    * ClassicTable readme and demo
    
    * fix images
    
    * fix card content
    
    * fix images
    
    * fix images
    
    * fix images
    
    * LabelDone fix iamges
    
    * fix type
    
    * ConfusionMatrix readme
    
    * ObjectClassesList readme
    
    * fix path
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update readme
    
    * launch.json
    
    * update Checkbox example
    
    * OneOf update demo and readme
    
    * NotificationBox readme and demo
    
    * DoneLabel readme and demo updated
    
    * updated ObjClassView readme and demo
    
    * update ObjectClassesList readme and demo
    
    * Empty - change video to gif
    
    * Table - change video to gif
    
    * fix readme text
    
    * fix env variables
    
    * small changes
    
    * small changes
    
    * small changes

--- 022_empty/src/main.py
+++ 022_empty/src/main.py
@@ -2,7 +2,7 @@ import os
 
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Card, Container, Empty
+from supervisely.app.widgets import Card, Container, Empty, OneOf, Select, Text
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
@@ -12,10 +12,17 @@ api = sly.Api()
 
 # initialize widgets we will use in UI
 empty = Empty()
+text = Text("Some text")
+items = [
+    Select.Item("item_1", content=Empty()),
+    Select.Item("item_2", content=text)
+]
+select = Select(items=items)
+one_of = OneOf(select)
 
 card = Card(
     title="Empty",
     content=empty,
 )
-layout = Container(widgets=[card])
+layout = Container(widgets=[select, one_of])
 app = sly.Application(layout=layout)
