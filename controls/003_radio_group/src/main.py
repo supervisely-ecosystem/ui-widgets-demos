9a3cceaba553f3e8df6f10aee8c5beed37031b18
almaz_macbook <almazgimaev@gmail.com>
2023-03-01 19:24:36 +0300

    Switch and RadioGroup - fix sessions error

--- controls/003_radio_group/src/main.py
+++ controls/003_radio_group/src/main.py
@@ -1,11 +1,17 @@
+import os
+
 import supervisely as sly
 from dotenv import load_dotenv
 from supervisely.app.widgets import RadioGroup, Text, OneOf, Container, Card
 
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-items=[
-    RadioGroup.Item(value="cat", label="Cat", content=Text(text='Cat says "Meow!"')), 
-    RadioGroup.Item(value="dog", label="Dog", content=Text(text='Dot says "Woof!"')), 
+api = sly.Api()
+
+items = [
+    RadioGroup.Item(value="cat", label="Cat", content=Text(text='Cat says "Meow!"')),
+    RadioGroup.Item(value="dog", label="Dog", content=Text(text='Dot says "Woof!"')),
     RadioGroup.Item(value="bird", label="Bird", content=Text(text='Bird says "Tweet!"')),
 ]
 
--- controls/004_switch/src/main.py
+++ controls/004_switch/src/main.py
@@ -1,12 +1,23 @@
+import os
+
 import supervisely as sly
+from dotenv import load_dotenv
 from supervisely.app.widgets import Container, Switch, OneOf, Text, Card
+
+# for convenient debug, has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 switch = Switch(on_content=Text("ON Conent"), off_content=Text("OFF content"))
 switch_one_of = OneOf(switch)
+
 
 @switch.value_changed
 def print_val(val):
     print(val)
 
-layout = Container(widgets=[Card(title="Switch", content=switch), Card(title="OneOf", content=switch_one_of)])
+
+layout = Container(
+    widgets=[Card(title="Switch", content=switch), Card(title="OneOf", content=switch_one_of)]
+)
 app = sly.Application(layout=layout)
