8d099f10079596057b46a855be64f6a4f7fe5de8
peixed351 <120389559+peixed351@users.noreply.github.com>
2023-03-09 17:05:55 +0300

    add_slider_widget (#67)

---
+++ misc/slider/src/main.py
@@ -0,0 +1,19 @@
+import os
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.app.widgets import Card, Container, Slider
+
+
+# for convenient debug, has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api = sly.Api()
+
+slider = Slider(value=[10, 50], step=5, range=True, show_stops=True)
+
+
+card = Card(title="Slider", content=Container([slider]))
+layout = Container(widgets=[card])
+app = sly.Application(layout=layout)
