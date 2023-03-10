a77090acebb5d9f915882eaecb1e5a9b4e5bd2a9
peixed351 <peixed351@gmail.com>
2023-03-10 13:14:47 +0300

    in progress

--- misc/task_logs/src/main.py
+++ misc/task_logs/src/main.py
@@ -9,12 +9,12 @@ from supervisely.app.widgets import Card, Container, TaskLogs
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+
 api = sly.Api()
 
-task_id = 28589
+task_id = 28628
 
 test = TaskLogs(task_id=task_id)
-
 
 card = Card(title="Task Logs", content=Container([test]))
 layout = Container(widgets=[card])
