e2290e57a2c9280a90dd6bcec04ecfad9cfcde35
peixed351 <peixed351@gmail.com>
2023-03-12 18:37:36 +0300

    add_image_slider

---
+++ misc/image_slider/src/main.py
@@ -0,0 +1,55 @@
+import os
+
+from dotenv import load_dotenv
+import supervisely as sly
+from supervisely.app.widgets import Card, Container, Text, ImageSlider, Button
+
+# for convenient debug, has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api = sly.Api()
+
+
+image_urls = [
+    "https://www.w3schools.com/howto/img_nature.jpg",
+    "https://www.quackit.com/pix/samples/18m.jpg",
+    "https://i.imgur.com/35pUPD2.jpg",
+    "https://i.imgur.com/fB2DBcM.jpeg",
+    "https://i.imgur.com/OpSj3JE.jpg",
+]
+
+
+image_slider = ImageSlider(data=image_urls, selectable=True, height=250)
+
+
+image_url = Text()
+image_index = Text()
+
+button_url = Button(text="Get image url")
+button_index = Button(text="Get image index")
+
+buttons_container = Container(
+    widgets=[button_url, button_index],
+    direction="vertical",
+)
+
+
+card = Card(
+    title="Image Slider",
+    content=Container([image_slider, image_url, image_index, buttons_container]),
+)
+
+layout = Container(widgets=[card])
+
+app = sly.Application(layout=layout)
+
+
+@button_url.click
+def get_url():
+    image_url.set(text=f"Image URL: {image_slider.get_preview_url()}", status="info")
+
+
+@button_index.click
+def get_index():
+    image_index.set(text=f"Image index on slider: {image_slider.get_preview_idx()}", status="info")
