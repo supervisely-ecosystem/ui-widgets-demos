7b5f17d4e0e7f2726fece58e32f32470896d06f1
peixed351 <peixed351@gmail.com>
2023-03-14 19:25:19 +0300

    in progress

--- misc/timeline/src/main.py
+++ misc/timeline/src/main.py
@@ -11,8 +11,46 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
+intervals = [
+    [1, 1],
+    [3, 9],
+    [10, 20],
+    [40, 70],
+    [80, 90],
+    [100, 300],
+    [320, 330],
+    [340, 345],
+    [400, 450],
+    [500, 540],
+    [650, 680],
+    [700, 750],
+    [900, 1100],
+    [1200, 1230],
+    [1240, 1243],
+]
 
-timeline = Timeline()
+colors = [
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+    "#99d162",
+]
+
+pointer_color = (255, 0, 127, 1)
+timeline = Timeline(
+    frames_count=1243, intervals=intervals, colors=colors, pointer_color=pointer_color
+)
 
 card = Card(
     # title="File Storage Upload",
