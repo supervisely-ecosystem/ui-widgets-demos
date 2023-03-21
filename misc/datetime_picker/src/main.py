b0a552b9c12aacc1417e6761f8bfbb490c6b845f
GoldenAnpu <sych.sergius@gmail.com>
2023-03-21 20:34:14 +0100

    initial

---
+++ misc/datetime_picker/src/main.py
@@ -0,0 +1,27 @@
+import os
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.app.widgets import Card, Container, Text, DateTimePicker
+
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api: sly.Api = sly.Api.from_env()
+
+text = Text()
+datetime_picker = DateTimePicker()
+
+card = Card(
+    "Date and Time Picker",
+    content=Container([datetime_picker, text]),
+)
+
+layout = Container(widgets=[card])
+app = sly.Application(layout=layout)
+
+
+@datetime_picker.value_changed
+def show_datetime(res):
+    info = f"Selected date and time: {res}"
+    text.set(text=info, status="info")
