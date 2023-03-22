e0615400781ee182afcdfaaf199411234a64dbdf
peixed351 <peixed351@gmail.com>
2023-03-22 12:21:49 +0300

    in progress

--- misc/cascader/src/main.py
+++ misc/cascader/src/main.py
@@ -2,7 +2,7 @@ import os
 
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Card, Container, Select, Cascader
+from supervisely.app.widgets import Card, Container, Cascader, Text
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
@@ -12,20 +12,39 @@ api = sly.Api()
 
 
 # initialize widgets we will use in UI
+
+pussy_black_cat = Cascader.Item(value="pussy cat", label="pussy cat")
+smooth_haired_cat = Cascader.Item(value="smooth-haired cat", label="smooth-haired cat")
+black_cat = Cascader.Item(
+    value="black cat", label="black cat", children=[pussy_black_cat, smooth_haired_cat]
+)
+white_cat = Cascader.Item(value="white cat", label="white cat")
+red_cat = Cascader.Item(value="red cat", label="red cat")
+
+angry_dog = Cascader.Item(value="angry dog", label="angry dog", disabled=True)
+kind_dog = Cascader.Item(value="kind dog", label="kind dog")
+
 animals = [
-    Cascader.Item(value="cat", label="cat"),
-    Cascader.Item(value="dog", label="dog"),
+    Cascader.Item(value="cat", label="cat", children=[black_cat, white_cat, red_cat]),
+    Cascader.Item(value="dog", label="dog", children=[angry_dog, kind_dog]),
     Cascader.Item(value="horse", label="horse"),
     Cascader.Item(value="sheep", label="sheep"),
 ]
 
 select_items = Cascader(items=animals)
 
+text = Text()
 
 card = Card(
     title="Cascader",
-    content=Container(widgets=[select_items]),
+    content=Container(widgets=[select_items, text]),
 )
 
 layout = Container(widgets=[card])
 app = sly.Application(layout=layout)
+
+
+@select_items.value_changed
+def show_item(res):
+    info = f"You choise: {res} item"
+    text.set(text=info, status="info")
