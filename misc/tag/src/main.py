5a3f77419d89a395e163aae80b12f1b9c06fbf64
peixed351 <peixed351@gmail.com>
2023-03-22 16:13:58 +0300

    add_tag

---
+++ misc/tag/src/main.py
@@ -0,0 +1,27 @@
+import os
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.app.widgets import Card, Container, Tag
+
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api: sly.Api = sly.Api.from_env()
+
+tag = Tag(text="Tag")
+
+all_tag_types = [tag]
+for tag_type in ["primary", "gray", "success", "warning", "danger"]:
+    curr_tag = Tag(text=f"Tag {tag_type}", type=tag_type)
+    all_tag_types.append(curr_tag)
+
+card = Card(
+    "Tag",
+    content=Container(widgets=all_tag_types, direction="horizontal"),
+)
+
+
+layout = Container(widgets=[card])
+app = sly.Application(layout=layout)
