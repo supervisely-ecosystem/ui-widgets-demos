59f3003d5930d2c34ad6a9d4ed5529843f2ed3cb
peixed351 <peixed351@gmail.com>
2023-04-21 17:03:53 +0300

    in progress

--- misc/image_compare/src/main.py
+++ misc/image_compare/src/main.py
@@ -2,16 +2,22 @@ import os
 
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Button, Card, Container, CompareImages, Text, GridGallery
+from supervisely.app.widgets import Button, Card, Container, CompareImages, Text, LabeledImage
 
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
-compare_images = CompareImages()
+left_image = LabeledImage()
+right_image = LabeledImage()
+# left_image.set(title="left image", image_url="https://i.imgur.com/4XE5Fe7.jpg")
+# right_image.set(title="right image", image_url="https://i.imgur.com/o9iUwji.jpg")
 
-# compare_images.set_right(title="right image", image_url="https://i.imgur.com/4XE5Fe7.jpg")
+compare_images = CompareImages(widget_left=left_image, widget_right=right_image)
+
+
+# compare_images.set_right(title="right image", image_url="https://i.imgur.com/o9iUwji.jpg")
 # compare_images.set_left(title="left image", image_url="https://i.imgur.com/4XE5Fe7.jpg")
 
 project_id = 14957
@@ -28,8 +34,12 @@ image_name_right = images_infos[1].name
 image_url_right = images_infos[1].full_storage_url
 image_ann_right = sly.Annotation.from_json(data=anns_infos[1].annotation, project_meta=project_meta)
 
-compare_images.set_left(title=image_name_left, image_url=image_url_left, ann=image_ann_left)
-compare_images.set_right(title=image_name_right, image_url=image_url_right, ann=image_ann_right)
+left_image.set(title=image_name_left, image_url=image_url_left, ann=image_ann_left)
+right_image.set(title=image_name_right, image_url=image_url_right, ann=image_ann_right)
+
+
+# compare_images.set_left(title=image_name_left, image_url=image_url_left, ann=image_ann_left)
+# compare_images.set_right(title=image_name_right, image_url=image_url_right, ann=image_ann_right)
 
 # compare_images.set_left(title="left image", image_url="https://i.imgur.com/o9iUwji.jpg")
 # compare_images.set_right(title="right image", image_url="https://i.imgur.com/RWoYlXf.jpg")
