f922d936a9b2a6daaed13f0085cfd872c88fe95e
almaz_macbook <almazgimaev@gmail.com>
2023-05-31 16:20:08 +0300

    updated type of content, added 3 examples

--- misc/docstring/src/main.py
+++ misc/docstring/src/main.py
@@ -1,9 +1,11 @@
-import os, requests
+import os
+
+import pandas as pd
+from dotenv import load_dotenv
 
 import supervisely as sly
-from dotenv import load_dotenv
-from supervisely.app.widgets import Card, Container, Docstring, Button
-
+from supervisely.api.image_api import ImageApi  # we will use ImageApi class docstring as examples
+from supervisely.app.widgets import Card, Container, Docstring
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
@@ -11,118 +13,41 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api()
 
-button_list = Button(text="Set list example")
-button_table = Button(text="Set table example")
-button_checkboxes = Button(text="Set checkboxes example")
-button_clean = Button(text="Clean content")
-buttons_container = Container(
-    widgets=[button_list, button_table, button_checkboxes, button_clean],
-    direction="horizontal",
-)
+docstring_example_1 = ImageApi.__doc__
 
-list_example = """<html>
-<body>
-
-<h2>Unordered List with Disc Bullets</h2>
-
-<ul style="list-style-type:disc;">
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-</ul>  
-
-</body>
-</html>"""
-
-
-table_example = """<html>
+docstring_example_2 = """<html>
 <head>
 <style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 5px;
-}
-th {
-  text-align: left;
-}
+table, th, td { border: 1px solid black; border-collapse: collapse; }
+th, td { padding: 5px; }
+th { text-align: left; }
 </style>
 </head>
 <body>
 
-<h2>Left-align Headings</h2>
-<p>To left-align the table headings, use the CSS text-align property.</p>
-
-<table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th> 
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-  <tr>
-    <td>John</td>
-    <td>Doe</td>
-    <td>80</td>
-  </tr>
+<table>
+  <tr> <th>Class name</th> <th>Images count</th> <th>Labels count</th> </tr>
+  <tr> <td>Lemon</td> <td>6</td> <td>6</td> </tr>
+  <tr> <td>Kiwi</td> <td>6</td> <td>20</td> </tr>
 </table>
-
 </body>
 </html>"""
 
-checkboxes_example = """<html>
-<body>
+docstring_example_3 = pd.DataFrame(
+    {"Class name": ["Lemon", "kiwi"], "Images": [6, 6], "Labels": ["6", "20"]}
+)
 
-<h2>Checkboxes</h2>
-<p>The <strong>input type="checkbox"</strong> defines a checkbox:</p>
+docstring_1 = Docstring(content=docstring_example_1)
 
-<form action="/action_page.php">
-  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-  <label for="vehicle1"> I have a bike</label><br>
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-  <label for="vehicle2"> I have a car</label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-  <label for="vehicle3"> I have a boat</label><br><br>
-  <input type="submit" value="Submit">
-</form> 
+docstring_2 = Docstring()
+docstring_2.set_content(content=docstring_example_2, is_html=True)
+docstring_3 = Docstring()
+docstring_3.set_content(content=docstring_example_3, is_dataframe=True)
 
-</body>
-</html>"""
+# docstrings = Container([docstring_1, docstring_2, docstring_3])
 
-docstring = Docstring(data=list_example)
-
-
-card = Card(title="Docstring", content=Container([docstring, buttons_container]))
-layout = Container(widgets=[card], direction="vertical")
+card_1 = Card(title="Docstring example 1", content=docstring_1)
+card_2 = Card(title="Docstring example 2", content=docstring_3)
+card_3 = Card(title="Docstring example 3", content=docstring_3)
+layout = Container(widgets=[card_1, card_2, card_3], direction="horizontal")
 app = sly.Application(layout=layout)
-
-
-@button_list.click
-def gmail_content():
-    docstring.set_value(list_example)
-
-
-@button_table.click
-def gmail_content():
-    docstring.set_value(table_example)
-
-
-@button_checkboxes.click
-def google_content():
-    docstring.set_value(checkboxes_example)
-
-
-@button_clean.click
-def clear():
-    docstring.set_value("")
