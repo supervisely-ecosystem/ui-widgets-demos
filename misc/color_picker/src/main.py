c63878574a61bf5449a52a8cf6fb7042c84bcec2
Ars Vladimirov <grokhael@gmail.com>
2023-05-31 18:11:30 +0600

    upd main

--- misc/color_picker/src/main.py
+++ misc/color_picker/src/main.py
@@ -11,21 +11,31 @@ if sly.is_development():
 api: sly.Api = sly.Api.from_env()
 
 
-text = Text()
+color_picker_hex = ColorPicker()
+color_picker_hex_a = ColorPicker(show_info=True, color_format='hex', show_alpha=True)
 
-color_picker = ColorPicker()
+color_picker_hsl = ColorPicker(show_info=True, color_format='hsl')
+color_picker_hsl_a = ColorPicker(show_info=True, color_format='hsl', show_alpha=True)
+
+color_picker_hsv = ColorPicker(color_format='hsv')
+color_picker_hsv_a = ColorPicker(color_format='hsv', show_info=True, show_alpha=True)
+
+color_picker_rgb = ColorPicker(color_format='rgb')
+color_picker_rgb_a = ColorPicker(color_format='rgb', show_info=True, show_alpha=True)
 
 card = Card(
     "Color Picker",
-    content=Container([color_picker, text]),
+    content=Container([
+        color_picker_hex,
+        color_picker_hex_a,
+        color_picker_hsl,
+        color_picker_hsl_a, 
+        color_picker_hsv,
+        color_picker_hsv_a,
+        color_picker_rgb, 
+        color_picker_rgb_a,
+    ]),
 )
-
 
 layout = Container(widgets=[card])
 app = sly.Application(layout=layout)
-
-
-@color_picker.value_changed
-def show_color(res):
-    color_info = f"Current color: {res}"
-    text.set(text=color_info, status="info")
