48c1380eacd6390ea17931f3253cb40d210f4df9
almaz <almazgimaev@gmail.com>
2024-06-18 15:38:36 +0200

    update demo

--- tables/007_tags_table/src/main.py
+++ tables/007_tags_table/src/main.py
@@ -2,7 +2,7 @@ import os
 
 import supervisely as sly
 from dotenv import load_dotenv
-from supervisely.app.widgets import Card, Container, TagsTable, Text
+from supervisely.app.widgets import Card, Container, InputNumber, TagsTable, Text, Button
 
 # for convenient debug, has no effect in production
 if sly.is_development():
@@ -11,12 +11,18 @@ if sly.is_development():
 
 api = sly.Api()
 
-project_id = 38857
+project_id = 38792
 tags_table = TagsTable(project_id=project_id)
 tags_text = Text("Selected tags: ")
-card = Card(title="TagsTable", content=Container([tags_table, tags_text]))
+input_number = InputNumber(controls=False)
+btn = Button("Set project ID")
+card = Card(title="TagsTable", content=Container([tags_table, tags_text, input_number, btn]))
 app = sly.Application(layout=card)
 
 @tags_table.value_changed
 def display_selected_tags(value):
     tags_text.text = f"Selected tags: {', '.join(value)}"
+
+@btn.click
+def click_btn():
+    tags_table.read_project_from_id(input_number.value)
