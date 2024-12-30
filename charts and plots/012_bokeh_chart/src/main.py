a754abf02bf67e1a8cb7305c95df1b74860f6c1b
almaz <almazgimaev@gmail.com>
2024-12-30 15:59:15 +0100

    update Bokeh widget demo

--- charts and plots/012_bokeh_chart/src/main.py
+++ charts and plots/012_bokeh_chart/src/main.py
@@ -1,25 +1,46 @@
 import supervisely as sly
 import random, os
-from supervisely.app.widgets import Card, Container, IFrame, Bokeh, Text, Button
+from supervisely.app.widgets import Card, Container, IFrame, Bokeh, Text, Button, InputNumber
 from dotenv import load_dotenv
 
-if sly.is_development():
-    load_dotenv("local.env")
-    load_dotenv(os.path.expanduser("~/supervisely.env"))
+# if sly.is_development():
+#     load_dotenv("local.env")
+#     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 
-def get_plot():
+def get_plot(idx=0):
     # data = sly.json.load_json_file("charts and plots/012_bokeh_chart/src/series.json")
     # x_coordinates = [point["x"] for point in data]
     # y_coordinates = [point["y"] for point in data]
     x_coordinates = [random.uniform(0, 10) for _ in range(100)]
     y_coordinates = [random.uniform(0, 10) for _ in range(100)]
-    plot = Bokeh.Circle(x_coordinates, y_coordinates, radii=0.05)
+    color = sly.color.rgb2hex(sly.color.random_rgb())
+    plot = Bokeh.Circle(
+        x_coordinates,
+        y_coordinates,
+        radii=0.05,
+    )
+    plot = Bokeh.Circle(
+        x_coordinates,
+        y_coordinates,
+        radii=0.05,
+        colors=[color] * len(x_coordinates),
+        legend_label=f"Plot {idx}",
+        plot_id=f"plot_{idx}",
+    )
     return plot
 
 
-plot = get_plot()
-bokeh = Bokeh(plots=[], x_axis_visible=True, y_axis_visible=True, grid_visible=True)
+# plot = get_plot()
+bokeh = Bokeh(
+    plots=[],
+    x_axis_visible=True,
+    y_axis_visible=True,
+    grid_visible=True,
+    show_legend=True,
+    legend_location="right",
+    legend_click_policy="hide",
+)
 # bokeh.clear()
 # bokeh.add_plots([plot])
 iframe = IFrame()
@@ -30,8 +51,12 @@ bokeh_card = Card(title="Chart", content=Container([iframe]))
 
 
 text = Text()
-btn = Button()
-preview_card = Card(title="Preview", content=Container([text, btn]))
+btn = Button("Add plots")
+
+size_input = InputNumber(min=0.01, value=0.05, step=0.01)
+size_btn = Button("Change size of dots", button_size="small")
+dots = Container([size_input, size_btn], direction=["horizontal"])
+preview_card = Card(title="Preview", content=Container([text, btn, dots]))
 
 charts = Container([bokeh_card], direction=["horizontal"])
 layout = Container(widgets=[charts, preview_card])
@@ -45,8 +70,16 @@ def on_click(selected_ids):
 
 @btn.click
 def on_click():
-    plot = get_plot()
+    plots = [get_plot(i) for i in range(3)]
     bokeh.clear()
-    bokeh.add_plots([plot])
+    bokeh.add_plots(plots)
     iframe.set(bokeh.html_route_with_timestamp, height="650px", width="100%")
     text.text = "Plot updated"
+
+
+@size_btn.click
+def on_click():
+    size = size_input.value
+    bokeh.update_radii(size)
+    iframe.set(bokeh.html_route_with_timestamp, height="650px", width="100%")
+    text.text = "Size of dots updated"
