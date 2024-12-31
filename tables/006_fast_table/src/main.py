a6adf7cac76428010d83a61ec9c3a6e8fc461876
almaz <almazgimaev@gmail.com>
2024-12-31 11:48:02 +0100

    FastTable: docs and example usage for update_cell_value method

--- tables/006_fast_table/src/main.py
+++ tables/006_fast_table/src/main.py
@@ -1,20 +1,14 @@
-import os
 import json
+import random
 import pandas as pd
 
 import supervisely as sly
-from dotenv import load_dotenv
+
 from supervisely.app.widgets import (
     Card,
     Container,
     FastTable,
 )
-
-# for convenient debug, has no effect in production
-load_dotenv("local.env")
-load_dotenv(os.path.expanduser("~/supervisely.env"))
-
-api = sly.Api()
 
 data = [["apple", "21"], ["banana", "15"]]
 
@@ -47,9 +41,15 @@ layout = Container(widgets=[card])
 app = sly.Application(layout=layout)
 
 
-@fast_table.row_click
-def handle_table_row(clicked_row: sly.app.widgets.FastTable.ClickedRow):
-    sly.app.show_dialog(
-        f"{clicked_row.row[0]}",
-        f"You clicked table row with idx={clicked_row.row_index} in source data",
-    )
+# @fast_table.row_click
+# def handle_table_row(clicked_row: sly.app.widgets.FastTable.ClickedRow):
+#     sly.app.show_dialog(
+#         f"{clicked_row.row[0]}",
+#         f"You clicked table row with idx={clicked_row.row_index} in source data",
+#     )
+
+
+@fast_table.cell_click
+def handle_table_cell(cell: sly.app.widgets.FastTable.ClickedCell):
+    new_value = str(random.randint(0, 100))
+    fast_table.update_cell_value(cell.row_index, cell.column_index, new_value)
